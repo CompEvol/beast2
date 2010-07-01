77d8a9d93f1e18a092adb63994ecdb64baedeb01
Alexei Drummond <alexei.drummond@gmail.com>
2010-07-01 03:27:09 +0000

    Added StateNode interface

---
+++ src/beast/core/StateNode.java
@@ -0,0 +1,26 @@
+package beast.core;
+
+/**
+ * This interface represents a node of the state. Concrete classes include Parameters and Trees.
+ *
+ * @author Alexei Drummond
+ */
+public interface StateNode {
+
+    /**
+     * @return a deep copy of this node in the state. This will generally be called only for stochastic nodes.
+     */
+    StateNode copy();
+
+    /**
+     * @return true if this node is acting as a random variable, false if this node is fixed and effectively data.
+     */
+    boolean isStochastic();
+
+    /**
+     * @param isStochastic true if this need should be treated as stochastic, false if this node should be fixed
+     *                     and treated as data
+     */
+    void setStochastic(boolean isStochastic);
+
+}
