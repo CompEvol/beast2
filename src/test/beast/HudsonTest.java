1aeec7e795ce8ad7a8572cf079a96ee4519a29bc
Remco Bouckaert <higgs.ml@gmail.com>
2010-07-08 22:48:24 +0000

    test Hudson - automatic test framework

---
+++ src/test/beast/HudsonTest.java
@@ -0,0 +1,14 @@
+package test.beast;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+public class HudsonTest extends TestCase {
+
+	   @Test
+	   public void test_returnEuro() {
+	      System.out.println("Test if Hudson Works") ;
+	      assertTrue(false) ;
+	   }
+
+}
