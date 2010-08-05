902e722251078aafeaace8e9591fd8f9de48ddd5
Joseph Heled <jheled@gmail.com>
2010-08-05 06:21:11 +0000

    Description and copyright text

--- src/beast/evolution/operators/BitFlipOperator.java
+++ src/beast/evolution/operators/BitFlipOperator.java
@@ -1,3 +1,27 @@
+/*
+* File BitFlipOperator.java
+*
+* Copyright (C) 2010 Joseph Heled jheled@gmail.com
+* This file is part of BEAST2.
+* See the NOTICE file distributed with this work for additional
+* information regarding copyright ownership and licensing.
+*
+* BEAST is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+*  BEAST is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with BEAST; if not, write to the
+* Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+* Boston, MA  02110-1301  USA
+*/
+
 package beast.evolution.operators;
 
 import beast.core.Operator;
@@ -11,7 +35,8 @@ import beast.util.Randomizer;
  *
  */
 
-@Description("bit flip operator bla bla")
+@Description("Flip one bit in an array of boolean bits. The hastings ratio is designed so that all subsets of vectors with the" +
+        " same number of 'on' bits are equiprobable.")
 public class BitFlipOperator extends Operator {
     public Input<Boolean> uniform = new Input<Boolean>("uniform", "when on, total probability of combinations with k" +
             " 'on' bits is equal. Otherwise uniform on all combinations");
