4dac0ccdd338a792a8781ad17707e0cc66f880b3
Remco Bouckaert <higgs.ml@gmail.com>
2011-03-24 00:21:26 +0000

    added operator list input editor

---
+++ src/beast/app/beauti/OperatorListInputEditor.java
@@ -0,0 +1,121 @@
+package beast.app.beauti;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import beast.app.draw.ListInputEditor;
+import beast.core.Operator;
+import beast.core.Plugin;
+import beast.core.StateNode;
+
+public class OperatorListInputEditor extends ListInputEditor {
+	private static final long serialVersionUID = 1L;
+	List<JTextField> m_textFields = new ArrayList<JTextField>(); 
+	List<Operator> m_operators = new ArrayList<Operator>();
+
+	@Override
+	public Class<?> type() {
+		return List.class;
+	}
+	@Override
+	public Class<?> baseType() {
+		return Operator.class;
+	}
+
+    /** add components to box that are specific for the plugin.
+     * By default, this just inserts a label with the plugin ID 
+     * @param itemBox box to add components to
+     * @param plugin plugin to add
+     */
+    protected void addPluginItem(Box itemBox, Plugin plugin) {
+        Operator operator = (Operator) plugin;
+        JLabel label = new JLabel(getLabel(operator));
+        
+        label.setBackground(Color.WHITE);
+        m_labels.add(label);
+        itemBox.add(label);
+
+        
+        
+        itemBox.add(Box.createHorizontalGlue());
+        JTextField weightEntry = new JTextField();
+        weightEntry.setToolTipText(operator.m_pWeight.getTipText());
+        weightEntry.setText(operator.m_pWeight.get() + "");
+        weightEntry.getDocument().addDocumentListener(new OperatorDocumentListener(operator, weightEntry));    
+        weightEntry.setMinimumSize(new Dimension(200, 16));
+        weightEntry.setMaximumSize(new Dimension(200, 20));
+        itemBox.add(weightEntry);
+        
+    }
+    
+    
+    /** class to set weight-input on an operator when it changes in the list **/
+    class OperatorDocumentListener implements DocumentListener {
+    	Operator m_operator;
+    	JTextField m_weightEntry;
+    	
+    	OperatorDocumentListener(Operator operator, JTextField weightEntry) {
+    		m_operator = operator;
+    		m_weightEntry = weightEntry;
+    		m_textFields.add(weightEntry); 
+    		m_operators.add(operator);
+    	}
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			processEntry();
+		}
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			processEntry();
+		}
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			processEntry();
+		}
+		void processEntry() {
+			try {
+				Double fWeight = Double.parseDouble(m_weightEntry.getText());
+				m_operator.m_pWeight.setValue(fWeight, m_operator);
+			} catch (Exception e) {
+				// ignore
+			}
+			validate();
+		}
+	};     
+    
+	@Override
+    protected void updateState() {
+		super.updateState();
+		for (int i = 0; i < m_textFields.size(); i++) {
+			m_textFields.get(i).setText(m_operators.get(i).m_pWeight.get() + "");
+			m_labels.get(i).setText(getLabel(m_operators.get(i)));
+		}
+    }
+	
+	String getLabel(Operator operator) {
+        String sName = operator.getID();
+        if (sName == null || sName.length() == 0) {
+            sName = operator.getClass().getName();
+            sName = sName.substring(sName.lastIndexOf('.') + 1);
+        }
+        sName  += ": ";
+        try {
+        	for (Plugin plugin2 : operator.listActivePlugins()) {
+        		if (plugin2 instanceof StateNode && ((StateNode) plugin2).m_bIsEstimated.get()) {
+        			sName += plugin2.getID() + " "; 
+        		}
+        	}
+        } catch (Exception e) {
+			// ignore
+		}
+        return sName;
+	}
+} // OperatorListInputEditor
