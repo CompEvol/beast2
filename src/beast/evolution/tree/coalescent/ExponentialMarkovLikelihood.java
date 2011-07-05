0e8d16deef8706c9f9a0068e44b6a82a66efeeb2
Remco Bouckaert <higgs.ml@gmail.com>
2011-07-05 08:40:28 +0000

    make exponential markov likelihood pass documentation test

--- src/beast/evolution/tree/coalescent/ExponentialMarkovLikelihood.java
+++ src/beast/evolution/tree/coalescent/ExponentialMarkovLikelihood.java
@@ -15,7 +15,7 @@ import beast.core.State;
 import beast.core.parameter.RealParameter;
 
 /** Ported from Beast 1.7 */
-@Description("A class that chains values in a parameter through the Gamma distribution. " +
+@Description("A class that produces a distribution chaining values in a parameter through the Gamma distribution. " +
 		"The value of a parameter is assumed to be Gamma distributed with mean as the previous value in the parameter. " +
 		"If a Jeffrey's prior is used, the first value is assumed to be distributed as 1/x, otherwise it is assumed to be uniform. " +
 		"Handy for population parameters. ")
@@ -24,7 +24,7 @@ public class ExponentialMarkovLikelihood extends Distribution {
 	public Input<Boolean> bIsJeffreysInput = new Input<Boolean>("jeffreys", "use Jeffrey's prior (default false)", false);
 	public Input<Boolean> bIsReverseInput = new Input<Boolean>("reverse", "parameter in reverse (default false)", false);
 	public Input<Double> fShapeInput = new Input<Double>("shape", "shape parameter of the Gamma distribution (default 1.0 = exponential distribution)", 1.0);
-	public Input<RealParameter> parameterInput = new Input<RealParameter>("parameter", "parameter", Validate.REQUIRED);
+	public Input<RealParameter> parameterInput = new Input<RealParameter>("parameter", "chain parameter to calculate distribution over", Validate.REQUIRED);
 	
     // **************************************************************
     // Private instance variables
