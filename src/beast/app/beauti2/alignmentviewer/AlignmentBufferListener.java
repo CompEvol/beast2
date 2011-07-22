ffc9d64992bd7a25cad6e7c43ae4f9f6e8895da8
Andrew Rambaut <a.rambaut@ed.ac.uk>
2011-07-22 15:57:14 +0000

    Removing UI stuff due to bloat concern.

--- src/beast/app/beauti2/alignmentviewer/AlignmentBufferListener.java
+++
@@ -1,9 +0,0 @@
-package beast.app.beauti2.alignmentviewer;
-
-/**
- * @author Andrew Rambaut
- * @version $Id: AlignmentBufferListener.java,v 1.1 2005/11/01 23:52:04 rambaut Exp $
- */
-public interface AlignmentBufferListener {
-    void alignmentChanged();
-}
