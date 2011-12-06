80439354ea7081e2e48dbb6f712d38c270dda427
Benjamin Redelings <benjamin.redelings@gmail.com>
2011-12-06 21:13:33 +0000

    Make slice sampling actually work.

--- src/beast/evolution/operators/SliceOperator.java
+++ src/beast/evolution/operators/SliceOperator.java
@@ -29,6 +29,7 @@ public class SliceOperator extends Operator {
 
     double windowSize = 1;
     Distribution g;
+
     public void initAndValidate() 
     {
 	totalDelta = 0.0;
@@ -48,45 +49,36 @@ public class SliceOperator extends Operator {
     {
 	return (x < X.getLower());
     }
-    
+
     boolean above_upper_bound(RealParameter X, double x)
     {
 	return (x > X.getUpper());
     }
 
-    Double evaluate(RealParameter X, double x)
+    public Distribution getEvaluatorDistribution() {
+	return g;
+    }
+
+    Double evaluate(Evaluator E)
+    {
+	return E.evaluate();
+    }
+
+    Double evaluate(Evaluator E, RealParameter X, double x)
     {
 	X.setValue(0,x);
-	try {
-	    return g.calculateLogP();
-	}
-	catch (Exception e)
-	{
-	    return  Double.NEGATIVE_INFINITY;
-	}
+	return evaluate(E);
     }
 
-    Double evaluate()
-    {
-	try {
-	    return g.calculateLogP();
-	}
-	catch (Exception e)
-	{
-	    return  Double.NEGATIVE_INFINITY;
-	}
-
-    }
-
-    void find_slice_boundaries_stepping_out(RealParameter X, double logy, double w,int m, Double L, Double R)
+    Double[] find_slice_boundaries_stepping_out(Evaluator E, RealParameter X, double logy, double w,int m)
     {
 	double x0 = X.getValue(0);
 	
 	assert in_range(X,x0);
 	
 	double u = Randomizer.nextDouble()*w;
-	L = x0 - u;
-	R = x0 + (w-u);
+	Double L = x0 - u;
+	Double R = x0 + (w-u);
 	
 	// Expand the interval until its ends are outside the slice, or until
 	// the limit on steps is reached.
@@ -95,51 +87,62 @@ public class SliceOperator extends Operator {
 	    int J = (int)Math.floor(Randomizer.nextDouble()*m);
 	    int K = (m-1)-J;
 	    
-	    while (J>0 && (!below_lower_bound(X,L)) && evaluate(X,L)>logy) {
+	    while (J>0 && (!below_lower_bound(X,L)) && evaluate(E,X,L)>logy) {
 		L -= w;
 		J--;
 	    }
 	    
-	    while (K>0 && (!above_upper_bound(X,R)) && evaluate(X,R)>logy) {
+	    while (K>0 && (!above_upper_bound(X,R)) && evaluate(E,X,R)>logy) {
 		R += w;
 		K--;
 	    }
 	}
 	else {
-	    while ((!below_lower_bound(X,L)) && evaluate(X,L)>logy)
+	    while ((!below_lower_bound(X,L)) && evaluate(E,X,L)>logy)
 		L -= w;
 	    
-	    while ((!above_upper_bound(X,R)) && evaluate(X,R)>logy)
+	    while ((!above_upper_bound(X,R)) && evaluate(E,X,R)>logy)
 		R += w;
 	}
 	
 	// Shrink interval to lower and upper bounds.
 	
-	if (below_lower_bound(X,L)) L = X.getLower();
-	if (above_upper_bound(X,R)) R = X.getUpper();
-	
+	if (below_lower_bound(X,L)) 
+	    L = X.getLower();
+	if (above_upper_bound(X,R)) 
+	    R = X.getUpper();
+
 	assert L < R;
+
+	Double[] range = {L,R};
+	return range;
     }
     
     // Does this x0 really need to be the original point?
     // I think it just serves to let you know which way the interval gets shrunk...
     
-    double search_interval(RealParameter X, Double L, Double R, double logy)
+    double search_interval(Evaluator E, double x0, RealParameter X, Double L, Double R, double logy)
     {
-	double x0 = X.getValue(0);
+	//	assert evaluate(E,x0) > evaluate(E,L) && evaluate(E,x0) > evaluate(E,R);
 
-	//  assert(g(x0) > g(L) && g(x0) > g(R));
-	assert evaluate(X,x0) >= logy;
+	assert evaluate(E,X,x0) >= logy;
 	assert L < R;
 	assert L <= x0 && x0 <= R;
 	
 	double L0 = L, R0 = R;
 	
+	double gx0 = evaluate(E,X,x0);  
+	assert logy < gx0;
+
+	double x1 = x0;
 	for(int i=0;i<200;i++)
 	{
-	    double x1 = L + Randomizer.nextDouble()*(R-L);
-	    
-	    double gx1 = evaluate(X,x1);
+	    x1 = L + Randomizer.nextDouble()*(R-L);
+	    double gx1 = evaluate(E,X,x1);
+
+	    //	    System.err.println("    L0 = " + L0 + "   x0 = " + x0 + "   R0 = " + R0 + "   gx0 = " + gx0);
+	    //	    System.err.println("    L  = " + L  + "   x1 = " + x1 + "   R  = " + R0 + "   gx1 = " + gx1);
+	    //	    System.err.println("    logy  = " + logy);
 	    
 	    if (gx1 >= logy) return x1;
 	    
@@ -149,42 +152,48 @@ public class SliceOperator extends Operator {
 		L = x1;
 	}
 	System.err.println("Warning!  Is size of the interval really ZERO?");
-	double logy_x0 = evaluate(X,x0);  
-	System.err.println("    L0 = " + L0 + "   x0 = " + x0 + "   R0 = " + R0);
-	System.err.println("    L  = " + L  + "                    R  = " + R);
-	System.err.println("    logy  = " + logy + "  logy_x0 = " + logy_x0 + "  logy_current = " + evaluate());
+	//	double logy_x0 = evaluate(E,X,x0);  
+	System.err.println("    L0 = " + L0 + "   x0 = " + x0 + "   R0 = " + R0 + "   gx0 = " + gx0);
+	System.err.println("    L  = " + L  + "   x1 = " + x1 + "   R  = " + R0 + "   gx1 = " + evaluate(E));
 	
 	return x0;
     }
 
+
+    @Override
+    public double proposal() {
+	return 0;
+    }
 
     /**
      * override this for proposals,
      * returns log of hastingRatio, or Double.NEGATIVE_INFINITY if proposal should not be accepted *
      */
     @Override
-    public double proposal() {
+    public double proposal(Evaluator E) {
 
 	int m = 100;
 
     	RealParameter X = parameterInput.get();
 
 	// Find the density at the current point
-	Double gx0 = evaluate();
+	Double gx0 = evaluate(E);
 
+	//	System.err.println("gx0 = " + gx0);
 	// Get the 1st element 
 	Double x0 = X.getValue(0);
+	//	System.err.println("x0 = " + x0);
 
 	// Determine the slice level, in log terms.
 	double logy = gx0 - Randomizer.nextExponential(1);
 
 	// Find the initial interval to sample from.
-	Double L=X.getLower();
-	Double R=X.getUpper();
-	find_slice_boundaries_stepping_out(X, logy, windowSize, m, L, R);
+	Double[] range = find_slice_boundaries_stepping_out(E, X, logy, windowSize, m);
+	Double L=range[0];
+	Double R=range[1];
 
 	// Sample from the interval, shrinking it on each rejection
-	double x_new = search_interval(X, L, R, logy);
+	double x_new = search_interval(E, x0, X, L, R, logy);
 	X.setValue(x_new);
 
 	if (n_learning_iterations > 0)
@@ -198,7 +207,9 @@ public class SliceOperator extends Operator {
 	    if (totalNumber > 3)
 	    {
 		W = 0.95*W + 0.05*W_predicted;
+		windowSize = W;
 	    }
+	    //	    System.err.println("W = " + W);
 	}
 
 	return Double.POSITIVE_INFINITY;
