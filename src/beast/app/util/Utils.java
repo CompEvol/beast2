7c40964aba9ba82cf528b4497b6c0bff310fa04e
Andrew Rambaut <a.rambaut@ed.ac.uk>
2011-12-22 13:44:15 +0000

    More consistent isMac() function.

--- src/beast/app/util/Utils.java
+++ src/beast/app/util/Utils.java
@@ -12,7 +12,7 @@ import java.util.*;
 /**
  * @author Andrew Rambaut
  * @author Alexei Drummond
-  */
+ */
 public class Utils {
 
     /**
@@ -64,8 +64,8 @@ public class Utils {
         final String f = System.getProperty("user.dir");
         return new File(f);
     }
-    
-    
+
+
     public static void loadUIManager() {
         boolean lafLoaded = false;
 
@@ -109,138 +109,139 @@ public class Utils {
             UIManager.put("SystemFont", new Font("Lucida Grande", Font.PLAIN, 13));
             UIManager.put("SmallSystemFont", new Font("Lucida Grande", Font.PLAIN, 11));
         }
-        
+
         try {
 
             if (!lafLoaded) {
                 UIManager.setLookAndFeel("javax.swing.plaf.metal");
-                		//UIManager.getSystemLookAndFeelClassName());
+                //UIManager.getSystemLookAndFeelClassName());
             }
         } catch (Exception e) {
         }
     }
 
     public static boolean isMac() {
-    	return !(isWindows() || isLinux());
-        //return jam.mac.Utils.isMacOSX();//System.getProperty("os.name").startsWith("Windows");
-     }
-    public static boolean isWindows() {
-       return System.getProperty("os.name").toLowerCase().startsWith("windows");
+        return System.getProperty("os.name").toLowerCase().startsWith("mac");
     }
+
+    public static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().startsWith("windows");
+    }
+
     public static boolean isLinux() {
         return System.getProperty("os.name").toLowerCase().startsWith("linux");
-     }
+    }
 
     public static File getLoadFile(String message) {
-    	return getLoadFile(message, null, null, (String[]) null);
+        return getLoadFile(message, null, null, (String[]) null);
     }
-    
+
     public static File getSaveFile(String message) {
-    	return getSaveFile(message, null, null, (String[]) null);
+        return getSaveFile(message, null, null, (String[]) null);
     }
-    
+
     public static File getLoadFile(String message, File defaultFileOrDir, String description, final String... extensions) {
-    	File [] files = getFile(message, true, defaultFileOrDir, false, description, extensions);
-    	if (files == null) {
-    		return null;
-    	} else {
-    		return files[0];
-    	}
+        File [] files = getFile(message, true, defaultFileOrDir, false, description, extensions);
+        if (files == null) {
+            return null;
+        } else {
+            return files[0];
+        }
     }
-    
+
     public static File getSaveFile(String message, File defaultFileOrDir, String description, final String... extensions) {
-    	File [] files = getFile(message, false, defaultFileOrDir, false, description, extensions);
-    	if (files == null) {
-    		return null;
-    	} else {
-    		return files[0];
-    	}
+        File [] files = getFile(message, false, defaultFileOrDir, false, description, extensions);
+        if (files == null) {
+            return null;
+        } else {
+            return files[0];
+        }
     }
 
     public static File [] getLoadFiles(String message, File defaultFileOrDir, String description, final String... extensions) {
-    	return getFile(message, true, defaultFileOrDir, true, description, extensions);
+        return getFile(message, true, defaultFileOrDir, true, description, extensions);
     }
-    
+
     public static File [] getSaveFiles(String message, File defaultFileOrDir, String description, final String... extensions) {
-    	return getFile(message, false, defaultFileOrDir, true, description, extensions);
+        return getFile(message, false, defaultFileOrDir, true, description, extensions);
     }
-    
+
     public static File [] getFile(String message, boolean bLoadNotSave, File defaultFileOrDir, boolean bAllowMultipleSelection, String description, final String... extensions) {
-    	if (isMac()) {
-	        java.awt.Frame frame = new java.awt.Frame();
-	        java.awt.FileDialog chooser = new java.awt.FileDialog(frame, message,
-	                (bLoadNotSave? java.awt.FileDialog.LOAD : java.awt.FileDialog.SAVE));
-	        if (defaultFileOrDir != null) {
-	        	if (defaultFileOrDir.isDirectory()) {
-	        		chooser.setDirectory(defaultFileOrDir.getAbsolutePath());
-	        	} else {
-	        		chooser.setFile(defaultFileOrDir.getAbsolutePath());
-	        	}
-	        }
-	        if (description != null) {
-	        	FilenameFilter filter = new FilenameFilter() {
-					@Override
-					public boolean accept(File dir, String name) {
-						for (int i = 0; i < extensions.length; i++) {
-							if (name.toLowerCase().endsWith(extensions[i].toLowerCase())) {
-								return true;
-							}
-						}
-						return false;
-					}
-				};
-				chooser.setFilenameFilter(filter);
-	        }
-	        
-	//        chooser.show();
-	        chooser.setVisible(true);
-	        if (chooser.getFile() == null) return null;
-	        java.io.File file = new java.io.File(chooser.getDirectory(), chooser.getFile());
-	        chooser.dispose();
-	        frame.dispose();
-           	return new File[]{file};
-    	} else {
-	        // No file name in the arguments so throw up a dialog box...
-	        java.awt.Frame frame = new java.awt.Frame();
-	        frame.setTitle(message);
-	        final JFileChooser chooser = new JFileChooser(defaultFileOrDir);
-	        chooser.setMultiSelectionEnabled(bAllowMultipleSelection);
-	        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-	        
-	        if (description != null) {
-	        	FileNameExtensionFilter filter = new FileNameExtensionFilter(description, extensions);
-	        	chooser.setFileFilter(filter);
-	        }
-	
-	        if (bLoadNotSave) {
-		        if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
-		            frame.dispose();
-		            if (bAllowMultipleSelection) {
-		            	return chooser.getSelectedFiles();
-		            } else {
-		            	if (chooser.getSelectedFile() == null) {
-		            		return null;
-		            	}
-		            	return new File[]{chooser.getSelectedFile()};
-		            }
-		        }
-	        } else {
-		        if (chooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
-		            frame.dispose();
-		            if (bAllowMultipleSelection) {
-		            	return chooser.getSelectedFiles();
-		            } else {
-		            	if (chooser.getSelectedFile() == null) {
-		            		return null;
-		            	}
-		            	return new File[]{chooser.getSelectedFile()};
-		            }
-		        }
-	        }
-    	}
+        if (isMac()) {
+            java.awt.Frame frame = new java.awt.Frame();
+            java.awt.FileDialog chooser = new java.awt.FileDialog(frame, message,
+                    (bLoadNotSave? java.awt.FileDialog.LOAD : java.awt.FileDialog.SAVE));
+            if (defaultFileOrDir != null) {
+                if (defaultFileOrDir.isDirectory()) {
+                    chooser.setDirectory(defaultFileOrDir.getAbsolutePath());
+                } else {
+                    chooser.setFile(defaultFileOrDir.getAbsolutePath());
+                }
+            }
+            if (description != null) {
+                FilenameFilter filter = new FilenameFilter() {
+                    @Override
+                    public boolean accept(File dir, String name) {
+                        for (int i = 0; i < extensions.length; i++) {
+                            if (name.toLowerCase().endsWith(extensions[i].toLowerCase())) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                };
+                chooser.setFilenameFilter(filter);
+            }
+
+            //        chooser.show();
+            chooser.setVisible(true);
+            if (chooser.getFile() == null) return null;
+            java.io.File file = new java.io.File(chooser.getDirectory(), chooser.getFile());
+            chooser.dispose();
+            frame.dispose();
+            return new File[]{file};
+        } else {
+            // No file name in the arguments so throw up a dialog box...
+            java.awt.Frame frame = new java.awt.Frame();
+            frame.setTitle(message);
+            final JFileChooser chooser = new JFileChooser(defaultFileOrDir);
+            chooser.setMultiSelectionEnabled(bAllowMultipleSelection);
+            chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+            if (description != null) {
+                FileNameExtensionFilter filter = new FileNameExtensionFilter(description, extensions);
+                chooser.setFileFilter(filter);
+            }
+
+            if (bLoadNotSave) {
+                if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+                    frame.dispose();
+                    if (bAllowMultipleSelection) {
+                        return chooser.getSelectedFiles();
+                    } else {
+                        if (chooser.getSelectedFile() == null) {
+                            return null;
+                        }
+                        return new File[]{chooser.getSelectedFile()};
+                    }
+                }
+            } else {
+                if (chooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
+                    frame.dispose();
+                    if (bAllowMultipleSelection) {
+                        return chooser.getSelectedFiles();
+                    } else {
+                        if (chooser.getSelectedFile() == null) {
+                            return null;
+                        }
+                        return new File[]{chooser.getSelectedFile()};
+                    }
+                }
+            }
+        }
         return null;
     }
 
-    
+
 
 }
