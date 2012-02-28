8cf6ad9013409047b9132074ca5b565efda35637
Joseph Heled <jheled@gmail.com>
2012-02-28 05:50:14 +0000

    Re-instate non-immediate reject version of narrow operator. remove extra condition from wide operator (not clear why it is there, not part of beast 1)

--- src/beast/evolution/operators/Exchange.java
+++ src/beast/evolution/operators/Exchange.java
@@ -77,7 +77,7 @@ public class Exchange extends TreeOperator {
      */
     @Override
     public double proposal() {
-        Tree tree = m_tree.get(this);
+        final Tree tree = m_tree.get(this);
 
         double fLogHastingsRatio = 0;
 
@@ -93,71 +93,78 @@ public class Exchange extends TreeOperator {
     /**
      * WARNING: Assumes strictly bifurcating beast.tree.
      */
-    public double narrow(Tree tree) {
-//        Alternative implementation that has less risk of rejection due to
-//    	  selecting an invalid initial node
-//    	
-//        final int nInternalNodes = tree.getInternalNodeCount();
-//        if (nInternalNodes <= 1) {
-//            return Double.NEGATIVE_INFINITY;
-//        }
-//
-//        Node iGrandParent = tree.getNode(nInternalNodes + 1 + Randomizer.nextInt(nInternalNodes));
-//        while (iGrandParent.m_left.isLeaf() && iGrandParent.m_right.isLeaf()) {
-//                iGrandParent = tree.getNode(nInternalNodes + 1 + Randomizer.nextInt(nInternalNodes));
-//        }
-//        
-//        Node iParent = iGrandParent.m_left;
-//        Node iUncle = iGrandParent.m_right;
-//        if (iParent.getHeight() < iUncle.getHeight()) {
-//                iParent = iGrandParent.m_right;
-//                iUncle = iGrandParent.m_left;
-//        }
-//        
-//        Node i = (Randomizer.nextBoolean() ? iParent.m_left : iParent.m_right);
-//        exchangeNodes(i, iUncle, iParent, iGrandParent);
-//        return 0;
-//        
+    public double narrow(final Tree tree) {
 
-        final int nNodes = tree.getNodeCount();
-        final Node root = tree.getRoot();
+        if( true ) {
+            //        Alternative implementation that has less risk of rejection due to
+            //    	  selecting an invalid initial node
+            //
+            final int nInternalNodes = tree.getInternalNodeCount();
+            if (nInternalNodes <= 1) {
+                return Double.NEGATIVE_INFINITY;
+            }
 
-        Node i = root;
+            Node iGrandParent = tree.getNode(nInternalNodes + 1 + Randomizer.nextInt(nInternalNodes));
+            while (iGrandParent.getLeft().isLeaf() && iGrandParent.getRight().isLeaf()) {
+                iGrandParent = tree.getNode(nInternalNodes + 1 + Randomizer.nextInt(nInternalNodes));
+            }
 
-        while (i.isRoot() || i.getParent().isRoot()) {
-            i = tree.getNode(Randomizer.nextInt(nNodes));
-        }
+            Node iParent = iGrandParent.getLeft();
+            Node iUncle = iGrandParent.getRight();
+            if (iParent.getHeight() < iUncle.getHeight()) {
+                iParent = iGrandParent.getRight();
+                iUncle = iGrandParent.getLeft();
+            }
 
-        final Node iParent = i.getParent();
-        final Node iGrandParent = iParent.getParent();
-        Node iUncle = iGrandParent.getLeft();
-        if (iUncle.getNr() == iParent.getNr()) {
-            iUncle = iGrandParent.getRight();
-            assert (iUncle.getNr() != iParent.getNr());
-        }
-        assert iUncle == getOtherChild(iGrandParent, iParent);
+            if( iParent.isLeaf() ) {
+                // tree with dated tips
+                return Double.NEGATIVE_INFINITY;
+            }
 
-        assert i.getHeight() <= iGrandParent.getHeight();
-
-        if (//i.getHeight() < iUncle.getHeight() && 
-                iUncle.getHeight() < iParent.getHeight()) {
+            final Node i = (Randomizer.nextBoolean() ? iParent.getLeft() : iParent.getRight());
             exchangeNodes(i, iUncle, iParent, iGrandParent);
             return 0;
         } else {
-            // Couldn't find valid narrow move on this beast.tree!!
-            return Double.NEGATIVE_INFINITY;
+
+            final int nNodes = tree.getNodeCount();
+
+            Node i = tree.getRoot();
+
+            while (i.isRoot() || i.getParent().isRoot()) {
+                i = tree.getNode(Randomizer.nextInt(nNodes));
+            }
+
+            final Node iParent = i.getParent();
+            final Node iGrandParent = iParent.getParent();
+            Node iUncle = iGrandParent.getLeft();
+            if (iUncle.getNr() == iParent.getNr()) {
+                iUncle = iGrandParent.getRight();
+                assert (iUncle.getNr() != iParent.getNr());
+            }
+            assert iUncle == getOtherChild(iGrandParent, iParent);
+
+            assert i.getHeight() <= iGrandParent.getHeight();
+
+            if (//i.getHeight() < iUncle.getHeight() &&
+                    iUncle.getHeight() < iParent.getHeight()) {
+                exchangeNodes(i, iUncle, iParent, iGrandParent);
+                return 0;
+            } else {
+                // Couldn't find valid narrow move on this beast.tree!!
+                return Double.NEGATIVE_INFINITY;
+            }
         }
     }
 
     /**
      * WARNING: Assumes strictly bifurcating beast.tree.
+     * @param tree
      */
-    public double wide(Tree tree) {
+    public double wide(final Tree tree) {
 
         final int nodeCount = tree.getNodeCount();
-        final Node root = tree.getRoot();
 
-        Node i = root;
+        Node i = tree.getRoot();
 
         while (i.isRoot()) {
             i = tree.getNode(Randomizer.nextInt(nodeCount));
@@ -174,8 +181,8 @@ public class Exchange extends TreeOperator {
         if ((iP != jP) && (i != jP) && (j != iP)
                 && (j.getHeight() < iP.getHeight())
                 && (i.getHeight() < jP.getHeight())
-                && ((iP.getHeight() < jP.getHeight() && i.getHeight() < j.getHeight()) ||
-                (iP.getHeight() > jP.getHeight() && i.getHeight() > j.getHeight()))
+//                && ((iP.getHeight() < jP.getHeight() && i.getHeight() < j.getHeight()) ||
+//                (iP.getHeight() > jP.getHeight() && i.getHeight() > j.getHeight()))
                 ) {
             exchangeNodes(i, j, iP, jP);
             // System.out.println("tries = " + tries+1);
