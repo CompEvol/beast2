932c8fa6d99e094496a9b05e6f4506f358652dbe
Jessie Wu <akaruiws@gmail.com>
2012-09-13 06:29:49 +0000

    Pieces that allow example xml files to run.

--- src/beast/math/distributions/ParametricDistribution.java
+++ src/beast/math/distributions/ParametricDistribution.java
@@ -77,7 +77,7 @@ public abstract class ParametricDistribution extends CalculationNode implements 
         final Double[][] sample = new Double[size][];
         for (int i = 0; i < sample.length; i++) {
             final double p = Randomizer.nextDouble();
-            sample[i] = new Double[]{inverseCumulativeProbability(p)};
+            sample[i] = new Double[]{inverseCumulativeProbability(p)+m_offset.get()};
         }
         return sample;
 
