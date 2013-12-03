abd20ed5b6e96ce93cf92ffeec37e69613a2a056
Remco Bouckaert <higgs.ml@gmail.com>
2013-12-03 00:30:22 +0000

    remove some unused classes

--- src/beast/core/parameter/DoubleMap.java
+++
@@ -1,9 +0,0 @@
-package beast.core.parameter;
-
-import beast.core.Description;
-
-@Description("Maps names to double values, e.g. taxon names to sample date")
-public class DoubleMap extends Map<Double> {
-	protected Class<?> mapType() {return Double.class;}
-
-}
--- src/beast/core/parameter/StringMap.java
+++
@@ -1,9 +0,0 @@
-package beast.core.parameter;
-
-import beast.core.Description;
-
-@Description("Maps names to string, e.g. taxon names to sequences")
-public class StringMap extends Map<String> {
-	protected Class<?> mapType() {return String.class;}
-
-}
