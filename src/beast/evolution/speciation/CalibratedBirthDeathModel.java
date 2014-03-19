7198240529decaf5c130be325ff23934c2badc02
Joseph Heled <jheled@gmail.com>
2014-03-19 13:07:06 +1300

    add missing init of 'outputs' when creating a copy

--- src/beast/evolution/speciation/CalibratedBirthDeathModel.java
+++ src/beast/evolution/speciation/CalibratedBirthDeathModel.java
@@ -393,7 +393,7 @@ public class CalibratedBirthDeathModel extends SpeciesTreeDistribution {
     }
 
     @Override
-    double calculateTreeLogLikelihood(final TreeInterface tree) {
+    public double calculateTreeLogLikelihood(final TreeInterface tree) {
         final double lam = birthRateInput.get().getArrayValue();
 
         double logL;
