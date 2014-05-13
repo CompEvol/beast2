ab9889b62c220770dca7d608c3ab6d4ab04df3be
rbouckaert <higgs.ml@gmail.com>
2014-05-13 15:44:46 +1200

    finalise release 2.1.3

--- src/beast/app/BEASTVersion.java
+++ src/beast/app/BEASTVersion.java
@@ -23,7 +23,7 @@ public class BEASTVersion extends Version {
 
     private static final String DATE_STRING = "2002-2014";
 
-    private static final boolean IS_PRERELEASE = true;
+    private static final boolean IS_PRERELEASE = false;
 
     private static final String REVISION = "$Rev: $";
 
