82e2395f056ab4e0089a7ea03ba78c581b6f42f9
Joseph Heled <jheled@gmail.com>
2014-09-22 20:40:03 +1200

    Add scale all flag. Fix incorrect hastings ratio

--- src/beast/evolution/tree/coalescent/SampleOffValues.java
+++ src/beast/evolution/tree/coalescent/SampleOffValues.java
@@ -21,6 +21,9 @@ public class SampleOffValues extends Operator {
     public Input<ParametricDistribution> distInput = new Input<ParametricDistribution>("dist",
             "distribution to sample from.", Input.Validate.REQUIRED);
 
+    public final Input<Boolean> scaleAll =
+            new Input<Boolean>("all", "if true, sample all off values in one go.", false);
+
     public void initAndValidate() {
     }
 
@@ -35,31 +38,50 @@ public class SampleOffValues extends Operator {
         final int offset = (data.getDimension() - 1) == idim ? 1 : 0;
         assert offset == 1 || data.getDimension() == idim : "" + idim + " (?+1) != " + data.getDimension();
 
-        // available locations for direct sampling
-        int[] loc = new int[idim];
-        int nLoc = 0;
+        double hr = Double.NEGATIVE_INFINITY;
 
-        for (int i = 0; i < idim; ++i) {
-            if (!indicators.getValue(i)) {
-                loc[nLoc] = i + offset;
-                ++nLoc;
-            }
-        }
-
-        if (nLoc > 0) {
-            final int index = loc[Randomizer.nextInt(nLoc)];
-            try {
-                final double val = distribution.inverseCumulativeProbability(Randomizer.nextDouble());
-                data.setValue(index, val);
-            } catch (Exception e) {
-                // some distributions fail on extreme values - currently gamma
-                return Double.NEGATIVE_INFINITY;
-                //throw new OperatorFailedException(e.getMessage());
+        if( scaleAll.get() ) {
+            for (int i = offset; i < idim; ++i) {
+                if( !indicators.getValue(i-offset) ) {
+                    try {
+                        final double val = distribution.inverseCumulativeProbability(Randomizer.nextDouble());
+                        hr += distribution.logDensity(data.getValue(i));
+                        data.setValue(i, val);
+                    } catch (Exception e) {
+                        // some distributions fail on extreme values - currently gamma
+                        return Double.NEGATIVE_INFINITY;
+                    }
+                }
             }
         } else {
-            // no non-active indicators
-            return Double.NEGATIVE_INFINITY;
+
+            // available locations for direct sampling
+            int[] loc = new int[idim];
+            int nLoc = 0;
+
+            for (int i = 0; i < idim; ++i) {
+                if( !indicators.getValue(i) ) {
+                    loc[nLoc] = i + offset;
+                    ++nLoc;
+                }
+            }
+
+            if( nLoc > 0 ) {
+                final int index = loc[Randomizer.nextInt(nLoc)];
+                try {
+                    final double val = distribution.inverseCumulativeProbability(Randomizer.nextDouble());
+                    hr = distribution.logDensity(data.getValue(index));
+                    data.setValue(index, val);
+                } catch (Exception e) {
+                    // some distributions fail on extreme values - currently gamma
+                    return Double.NEGATIVE_INFINITY;
+                    //throw new OperatorFailedException(e.getMessage());
+                }
+            } else {
+                // no non-active indicators
+                //return Double.NEGATIVE_INFINITY;
+            }
         }
-        return 0;
+        return hr;
     }
 }
