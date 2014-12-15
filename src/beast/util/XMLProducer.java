CompEvol/beast2

commit 04980b5a6edc9b1a64719fafc6a39d3ef98029bf

Give feedback when pre-burnin > 0 closes #225

src/beast/util/XMLProducer.java
@@ -29,6 +29,7 @@ package beast.util;
 
 import beast.core.BEASTInterface;
 import beast.core.Input;
+
 import org.w3c.dom.*;
 import org.xml.sax.InputSource;
 
@@ -38,11 +39,14 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.lang.reflect.Field;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * converts MCMC plug in into XML, i.e. does the reverse of XMLParser
@@ -129,13 +133,17 @@ public class XMLProducer extends XMLParser {
             		+ sEndBeast;
 
             sXML = sXML.replaceAll("xmlns=\"http://www.w3.org/TR/xhtml1/strict\"", "");
+            
+            sXML = dedupName(sXML);
+            sXML = sortTags(sXML);
+            
 
             //insert newlines in alignments
             int k = sXML.indexOf("<data ");
             StringBuffer buf2 = new StringBuffer(sXML); 
             while (k > 0) {
             	while (sXML.charAt(k) != '>') {
-            		if (sXML.charAt(k) == ' ') {
+            		if (sXML.charAt(k) == ' ' && !sXML.startsWith("idref", k+1)) {
             			buf2.setCharAt(k, '\n');
             		}
             		k++;
@@ -151,7 +159,189 @@ public class XMLProducer extends XMLParser {
         }
     } // toXML
 
-    /**
+    // ensure attributes are ordered so that id goes first, then spec, then remainder of attributes
+    private String sortTags(String sXML) {
+    	//if (true) return sXML;
+    	String [] strs = sXML.split("<");
+    	StringBuilder bf = new StringBuilder();
+    	bf.append(strs[0]);
+    	for (int i = 1; i < strs.length; i++) {
+    		String str = strs[i];
+    		String [] ss = str.split(">");
+    		boolean [] isShortEnd = new  boolean[ss.length];
+    		for (int j = 0; j < ss.length; j++) {
+    			if (ss[j].endsWith("/")) {
+    				isShortEnd[j] = true;
+    				ss[j] = ss[j].substring(0,  ss[j].length() - 1);
+    			}
+    		}
+   			String [] strs2 = split(ss[0]);
+   			if (str.contains("data")) {
+   				int k = 3;
+   				k++;
+   			}
+   			int iSpec = 0;
+   			while (iSpec < strs2.length && !strs2[iSpec].startsWith("spec=")) {
+   				iSpec++;
+   			}
+   			int iID = 0;
+   			while (iID < strs2.length && !strs2[iID].startsWith("id=")) {
+   				iID++;
+   			}
+			bf.append('<');
+			if (strs2[0] != null)
+				bf.append(strs2[0]);
+			if (iID < strs2.length) {
+				bf.append(' ');
+				bf.append(strs2[iID]);
+			}
+			if (iSpec < strs2.length) {
+				bf.append(' ');
+				bf.append(strs2[iSpec]);
+			}
+			for (int j = 1; j < strs2.length; j++) {
+				if (j != iID && j != iSpec) {
+	    			bf.append(' ');
+	    			if (strs2[j] != null)
+	    				bf.append(strs2[j]);
+				}
+			}
+			for (int k = 1; k < ss.length; k++) {
+				if (isShortEnd[k-1]) {
+					bf.append('/');
+				}
+				bf.append('>');
+				bf.append(ss[k]);
+			}
+    	}
+		return bf.toString();
+	}
+    
+    // since str.split(" "): does not match trailing spaces, we need to split by hand
+    String [] split(String str) {
+    	List<String> s = new ArrayList<>();
+    	StringBuilder buf = new StringBuilder();
+    	int i = 0;
+    	while (i < str.length()) {
+    		char c = str.charAt(i);
+    		if (c == ' ') {
+    			s.add(buf.toString());
+    			buf = new StringBuilder();
+    		} else {
+    			buf.append(c);
+    		}
+    		i++;
+    	}
+		s.add(buf.toString());
+    	return s.toArray(new String []{});
+    }
+
+	private String dedupName(String sXML) {
+        // replace <$x name="$y" idref="$z"/> and <$x idref="$z" name="$y"/> 
+        // with <$y idref="$z"/>
+        StringBuilder sb = new StringBuilder();
+        int i = -1;
+        while (++i < sXML.length()) {
+        	char c = sXML.charAt(i);
+        	if (c == '<') {
+                StringBuilder tag = new StringBuilder();
+        		tag.append(c);
+        		while (((c = sXML.charAt(++i)) != ' ') && (c != '/') && c != '>') {
+        			tag.append(c);
+        		}
+        		if (c != '/' && c != '>') {
+                    StringBuilder tag2 = new StringBuilder();
+            		while ((c = sXML.charAt(++i)) != '=') {
+            			tag2.append(c);
+            		}
+            		if (tag2.toString().equals("name")) {
+                        ++i;
+                        StringBuilder value2 = new StringBuilder();
+                		while ((c = sXML.charAt(++i)) != '"') {
+                			value2.append(c);
+                		}
+                        StringBuilder tag3 = new StringBuilder();
+                        ++i;
+                		while (((c = sXML.charAt(++i)) != '=') && (c != '/') && (c != '>')) {
+                			tag3.append(c);
+                		}
+                		if (c != '/' && c != '>' && tag3.toString().equals("idref")) {
+                			tag3.append(c);
+                			tag3.append(sXML.charAt(++i));
+                    		while ((c = sXML.charAt(++i)) != '"') {
+                    			tag3.append(c);
+                    		}
+                    		sb.append('<');
+                			sb.append(value2);                			
+                    		sb.append('=');
+                    		sb.append(tag3);
+                    		sb.append("/>");
+                    		while ((c = sXML.charAt(++i)) != '>') {}
+                		} else {
+                			sb.append(tag);
+                			sb.append(' ');
+                			sb.append(tag2);
+                			sb.append("=\"");
+                			sb.append(value2);                			
+                			sb.append('"');
+                			sb.append(tag3);
+                			sb.append(c);
+                		}
+            		} else if (tag2.toString().equals("idref")) {
+            			tag2.append(c);
+            			tag2.append(sXML.charAt(++i));
+                		while (((c = sXML.charAt(++i)) != ' ') && (c != '/') && c != '>') {
+                			tag2.append(c);
+                		}
+                		if (c != '/' && c != '>') {
+                            StringBuilder tag3 = new StringBuilder();
+                    		while ((c = sXML.charAt(++i)) != '=') {
+                    			tag3.append(c);
+                    		}
+                    		if (tag3.toString().equals("name")) {
+                                ++i;
+                                StringBuilder value2 = new StringBuilder();
+                        		while ((c = sXML.charAt(++i)) != '"') {
+                        			value2.append(c);
+                        		}
+                        		sb.append('<');
+                    			sb.append(value2);                			
+                        		sb.append(' ');
+                        		sb.append(tag2);
+                        		sb.append("/>");
+                        		while ((c = sXML.charAt(++i)) != '>') {}
+                    		} else {
+                    			sb.append(tag);
+                    			sb.append(' ');
+                    			sb.append(tag2);
+                    			sb.append(' ');
+                    			sb.append(tag3);
+                    			sb.append(c);
+                    		}
+                		} else {
+                			sb.append(tag);
+                			sb.append(' ');
+                			sb.append(tag2);
+                			sb.append(c);
+                		}
+            		} else {
+            			sb.append(tag);
+            			sb.append(' ');
+            			sb.append(tag2);                			
+            			sb.append(c);
+            		}
+        		} else {
+        			sb.append(tag);
+        			tag.append(c);
+        		}
+        	} else {
+        		sb.append(c);
+        	}
+        }
+        return sb.toString();
+	}
+
+	/**
      * like toXML() but without the assumption that plugin is Runnable *
      */
     public String modelToXML(BEASTInterface plugin) {

build.xml
@@ -422,7 +422,7 @@
     </target>
 
     <!-- Define the appbundler task -->
-    <taskdef name="bundleapp" classname="com.oracle.appbundler.AppBundlerTask" classpath="../beast2/lib/appbundler-1.0.jar"/>
+    <taskdef name="bundleapp" classname="com.oracle.appbundler.AppBundlerTask" classpath="../beast2/lib/bundler/appbundler-1.0.jar"/>
 
     <!-- Create the app bundle -->
     <target name="mac">

src/beast/core/MCMC.java
@@ -367,6 +367,9 @@ public class MCMC extends Runnable {
      */
     protected void doLoop() throws Exception {
         int corrections = 0;
+        if (burnIn > 0) {
+        	Log.warning.println("Please wait while BEAST takes " + burnIn + " pre-burnin samples");
+        }
         for (int sampleNr = -burnIn; sampleNr <= chainLength; sampleNr++) {
             final int currentState = sampleNr;
 

src/beast/evolution/tree/TraitSet.java
@@ -44,7 +44,7 @@ public class TraitSet extends BEASTObject {
     /**
      * String values of taxa in order of taxons in alignment*
      */
-    String[] taxonValues;
+    protected String[] taxonValues;
     
     /**
      * double representation of taxa value *

templates/SubstModels.xml
@@ -889,7 +889,7 @@
                 <distr id="popPriorDist.EBSP" mean="@populationMean.alltrees" spec="Exponential"/>
             </prior>
 
-            <logger id="EBSPLogger" fileName='EBSP.$(seed).log' logEvery='5000'>
+            <logger id="EBSPLogger" fileName='EBSP.$(seed).log' logEvery='5000' model="@demographic.alltrees">
                 <log idref='demographic.alltrees'/>
             </logger>
             <log id='sumIndicators' spec='beast.core.util.Sum' arg='@indicators.alltrees'/>

commit a37da5f33e24301cb468f32cc5a796b248c5986b

suppress XML beautification code (#252) introduced accidentally in 04980b5a6edc9b1a64719fafc6a39d3ef98029bf

src/beast/util/XMLProducer.java
@@ -134,8 +134,8 @@ public class XMLProducer extends XMLParser {
 
             sXML = sXML.replaceAll("xmlns=\"http://www.w3.org/TR/xhtml1/strict\"", "");
             
-            sXML = dedupName(sXML);
-            sXML = sortTags(sXML);
+            //sXML = dedupName(sXML);
+            //sXML = sortTags(sXML);
             
 
             //insert newlines in alignments
@@ -176,10 +176,6 @@ public class XMLProducer extends XMLParser {
     			}
     		}
    			String [] strs2 = split(ss[0]);
-   			if (str.contains("data")) {
-   				int k = 3;
-   				k++;
-   			}
    			int iSpec = 0;
    			while (iSpec < strs2.length && !strs2[iSpec].startsWith("spec=")) {
    				iSpec++;
@@ -212,6 +208,9 @@ public class XMLProducer extends XMLParser {
 				}
 				bf.append('>');
 				bf.append(ss[k]);
+			}
+			if (ss.length == 1 && str != null && str.endsWith(">")) {
+				bf.append('>');
 			}
     	}
 		return bf.toString();