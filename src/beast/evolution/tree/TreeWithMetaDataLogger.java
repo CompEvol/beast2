ead3f5363414ee1e420d39b8935820edcd17c566
rbouckaert <higgs.ml@gmail.com>
2015-03-03 16:03:48 +1300

    issue #284 allow logging multiple metadata items with a tree

--- src/beast/evolution/tree/TreeWithMetaDataLogger.java
+++ src/beast/evolution/tree/TreeWithMetaDataLogger.java
@@ -3,6 +3,8 @@ package beast.evolution.tree;
 import java.io.PrintStream;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import beast.core.Description;
 import beast.core.Function;
@@ -18,12 +20,11 @@ import beast.evolution.branchratemodel.BranchRateModel;
 public class TreeWithMetaDataLogger extends BEASTObject implements Loggable {
     public Input<Tree> treeInput = new Input<Tree>("tree", "tree to be logged", Validate.REQUIRED);
     // TODO: make this input a list of valuables
-    public Input<Function> parameterInput = new Input<Function>("metadata", "meta data to be logged with the tree nodes");
+    public Input<List<Function>> parameterInput = new Input<List<Function>>("metadata", "meta data to be logged with the tree nodes",new ArrayList<>());
     public Input<BranchRateModel.Base> clockModelInput = new Input<BranchRateModel.Base>("branchratemodel", "rate to be logged with branches of the tree");
     public Input<Boolean> substitutionsInput = new Input<Boolean>("substitutions", "report branch lengths as substitutions (branch length times clock rate for the branch)", false);
     public Input<Integer> decimalPlacesInput = new Input<Integer>("dp", "the number of decimal places to use writing branch lengths and rates, use -1 for full precision (default = full precision)", -1);
 
-    String metaDataLabel;
     
     boolean someMetaDataNeedsLogging;
     boolean substitutions = false;
@@ -32,15 +33,12 @@ public class TreeWithMetaDataLogger extends BEASTObject implements Loggable {
 
     @Override
     public void initAndValidate() throws Exception {
-        if (parameterInput.get() == null && clockModelInput.get() == null) {
+        if (parameterInput.get().size() == 0 && clockModelInput.get() == null) {
         	someMetaDataNeedsLogging = false;
         	return;
             //throw new Exception("At least one of the metadata and branchratemodel inputs must be defined");
         }
     	someMetaDataNeedsLogging = true;
-        if (parameterInput.get() != null) {
-            metaDataLabel = ((BEASTObject) parameterInput.get()).getID() + "=";
-        }
     	// without substitution model, reporting substitutions == reporting branch lengths 
         if (clockModelInput.get() != null) {
         	substitutions = substitutionsInput.get();
@@ -66,9 +64,11 @@ public class TreeWithMetaDataLogger extends BEASTObject implements Loggable {
     public void log(int nSample, PrintStream out) {
         // make sure we get the current version of the inputs
         Tree tree = (Tree) treeInput.get().getCurrent();
-        Function metadata = parameterInput.get();
-        if (metadata != null && metadata instanceof StateNode) {
-            metadata = ((StateNode) metadata).getCurrent();
+        List<Function> metadata = parameterInput.get();
+        for (int i = 0; i < metadata.size(); i++) {
+        	if (metadata.get(i) instanceof StateNode) {
+        		metadata.set(i, ((StateNode) metadata.get(i)).getCurrent());
+        	}
         }
         BranchRateModel.Base branchRateModel = clockModelInput.get();
         // write out the log tree with meta data
@@ -95,14 +95,14 @@ public class TreeWithMetaDataLogger extends BEASTObject implements Loggable {
         }
     }
 
-    String toNewick(Node node, Function metadata, BranchRateModel.Base branchRateModel) {
+    String toNewick(Node node, List<Function> metadataList, BranchRateModel.Base branchRateModel) {
         StringBuffer buf = new StringBuffer();
         if (node.getLeft() != null) {
             buf.append("(");
-            buf.append(toNewick(node.getLeft(), metadata, branchRateModel));
+            buf.append(toNewick(node.getLeft(), metadataList, branchRateModel));
             if (node.getRight() != null) {
                 buf.append(',');
-                buf.append(toNewick(node.getRight(), metadata, branchRateModel));
+                buf.append(toNewick(node.getRight(), metadataList, branchRateModel));
             }
             buf.append(")");
         } else {
@@ -110,26 +110,32 @@ public class TreeWithMetaDataLogger extends BEASTObject implements Loggable {
         }
         if (someMetaDataNeedsLogging) {
 	        buf.append("[&");
-	        if (metadata != null) {
-	            buf.append(metaDataLabel);
-	            if (metadata instanceof Parameter<?>) {
-	            	Parameter p = (Parameter) metadata;
-	            	int dim = p.getMinorDimension1();
-	            	if (dim > 1) {
-		            	buf.append('{');
-		            	for (int i = 0; i < dim; i++) {
-			            	buf.append(p.getMatrixValue(node.labelNr, i));
-			            	if (i < dim - 1) {
-				            	buf.append(',');
+	        if (metadataList.size() > 0) {
+	        	for (Function metadata : metadataList) {
+		            buf.append(((BEASTObject)metadata).getID());
+		            buf.append('=');
+		            if (metadata instanceof Parameter<?>) {
+		            	Parameter p = (Parameter) metadata;
+		            	int dim = p.getMinorDimension1();
+		            	if (dim > 1) {
+			            	buf.append('{');
+			            	for (int i = 0; i < dim; i++) {
+				            	buf.append(p.getMatrixValue(node.labelNr, i));
+				            	if (i < dim - 1) {
+					            	buf.append(',');
+				            	}
 			            	}
+			            	buf.append('}');
+		            	} else {
+			            	buf.append(metadata.getArrayValue(node.labelNr));
 		            	}
-		            	buf.append('}');
-	            	} else {
+		            } else {
 		            	buf.append(metadata.getArrayValue(node.labelNr));
-	            	}
-	            } else {
-	            	buf.append(metadata.getArrayValue(node.labelNr));
-	            }
+		            }
+		            if (metadataList.indexOf(metadata) < metadataList.size() - 1) {
+		            	buf.append(",");
+		            }
+	        	}
 	            if (branchRateModel != null) {
 	                buf.append(",");
 	            }
