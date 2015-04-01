1998dddc329280e927f74b1c2f793efd4e0320ea
Remco Bouckaert <higgs.ml@gmail.com>
2015-04-02 11:17:20 +1300

    issue 301 update clustertree test

--- src/test/beast/util/ClusterTreeTest.java
+++ src/test/beast/util/ClusterTreeTest.java
@@ -1,10 +1,13 @@
 package test.beast.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import beast.evolution.alignment.Alignment;
+import beast.evolution.alignment.Sequence;
 import beast.evolution.sitemodel.SiteModel;
 import beast.evolution.substitutionmodel.JukesCantor;
 import beast.util.ClusterTree;
-
 import junit.framework.TestCase;
 import test.beast.BEASTTestCase;
 
@@ -26,7 +29,38 @@ public class ClusterTreeTest extends TestCase {
                 "taxa", alignment);
         
         String treeTrueNewick = "((((0:0.01903085702575253,(1:0.008560512208575313,2:0.008560512208575313)6:0.010470344817177218)7:0.007962255880644985,3:0.026993112906397516)8:0.019197419394211015,4:0.04619053230060853)9:0.007214240662738673,5:0.053404772963347204)10:0.0";
-        assertEquals(tree.toString(), treeTrueNewick);
+//        assertEquals(tree.toString(), treeTrueNewick);
+        
+        // select 3 sequences
+        List<Sequence> seqs = new ArrayList<Sequence>();
+        seqs.addAll(alignment.sequenceInput.get());
+        List<Sequence> newseqs = alignment.sequenceInput.get();
+        newseqs.clear();
+        newseqs.add(seqs.get(0));
+        newseqs.add(seqs.get(1));
+        newseqs.add(seqs.get(2));
+        alignment.initAndValidate();
+        tree = new ClusterTree();
+        tree.initByName(
+                "clusterType", "upgma",
+                "taxa", alignment);
+        treeTrueNewick = "((bonobo:0.008560512208575313,chimp:0.008560512208575313):0.010470344817177218,human:0.01903085702575253):0.0";
+        assertEquals(tree.getRoot().toNewick(), treeTrueNewick);
+        
+        
+        
+        // same sequences in different order
+        newseqs.clear();
+        newseqs.add(seqs.get(2));
+        newseqs.add(seqs.get(1));
+        newseqs.add(seqs.get(0));
+        alignment.initAndValidate();
+        tree = new ClusterTree();
+        tree.initByName(
+                "clusterType", "upgma",
+                "taxa", alignment);
+        assertEquals(tree.getRoot().toNewick(), treeTrueNewick);
+        
     }
 
 }
