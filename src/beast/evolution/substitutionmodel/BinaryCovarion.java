18b91b2e3ceeee056f0e8e7708410659fee287fa
Remco Bouckaert <higgs.ml@gmail.com>
2015-08-10 08:55:26 +1200

    closes #398 add mode input to Covarian allowing it to become reversible/using alternative parameterisation

--- src/beast/evolution/substitutionmodel/BinaryCovarion.java
+++ src/beast/evolution/substitutionmodel/BinaryCovarion.java
@@ -4,6 +4,7 @@ import beast.core.Description;
 import beast.core.Input;
 import beast.core.Input.Validate;
 import beast.core.parameter.RealParameter;
+import beast.core.util.Log;
 import beast.evolution.datatype.DataType;
 import beast.evolution.datatype.TwoStateCovarion;
 
@@ -16,21 +17,63 @@ import beast.evolution.datatype.TwoStateCovarion;
  * p1	1 - p0, the equilibrium frequency of one states
  * f0	the equilibrium frequency of slow rate class
  * f1	1 - f0, the equilibrium frequency of fast rate class
- * s	the rate of switching
+ * s, s1, s2 the rate of switching
  * <p/>
- * then the (unnormalized) instantaneous rate matrix (unnormalized Q) should be
+ * then the (unnormalized) instantaneous rate matrix (unnormalized Q) should be (depending on mode)
  * <p/>
+ * 
+ * mode = BEAST -- using classic BEAST implementation, reversible iff hfrequencies = (0.5, 0.5)
+ * FLAGS: reversible = false, TSParameterisation = false
+ * 
  * [ -(a*p1)-s ,   a*p1    ,    s   ,   0   ]
  * [   a*p0    , -(a*p0)-s ,    0   ,   s   ]
  * [    s      ,     0     ,  -p1-s ,  p1   ]
  * [    0      ,     s     ,    p0  , -p0-s ]
+ *
+ * equilibrium frequencies
+ * [ p0 * f0, p1, * f0, p0 * f1, p1, * f1 ]
+ *
+ * mode = REVERSIBLE -- brings in hfrequencies in rate matrix
+ * reversible = true, TSParameterisation = false
+ * [ - , a , s , 0 ]
+ * [ a , - , 0 , s ]
+ * [ s , 0 , - , 1 ]
+ * [ 0 , s , 1 , - ]
+ * 
+ * which with frequencies becomes
+ * 
+ * [ -(a*p1*f0)-s*f0 ,   a*p1*f0       ,    s*f0      ,   0         ]
+ * [   a*p0*f0       , -(a*p0*f0)-s*f0 ,    0         ,   s*f0      ]
+ * [    s*f1         ,     0           ,  -p1*f1-s*f1 ,  p1*f1      ]
+ * [    0            ,     s*f1        ,    p0*f1     , -p0*f1-s*f1 ]
+ * 
+ * equilibrium frequencies
+ * [ p0 * f0, p1, * f0, p0 * f1, p1, * f1 ]
+ * 
+ * mode = TUFFLEYSTEEL uses alternative parameterisation: hfrequencies is ignored, and switch parameter is set to dimension = 2
+ * [ -(a*p1)-s1 ,   a*p1     ,    s1   ,   0    ]
+ * [   a*p0     , -(a*p0)-s1 ,    0    ,   s1   ]
+ * [    s2      ,     0      ,  -p1-s2 ,  p1    ]
+ * [    0       ,     s2     ,    p0   , -p0-s2 ]
+ *
+ * equilibrium frequencies
+ * [ f0 * s2/(s1+s2), f1, * s2/(s1+s2), f0 * s1/(s1+s2), f1, * s1/(s1+s2) ]
+ *
+ *
+ * Note: to use Tuffley & Steel's methods, set a = 0.
  */
 @Description("Covarion model for Binary data")
 public class BinaryCovarion extends GeneralSubstitutionModel {
     public Input<RealParameter> alphaInput = new Input<RealParameter>("alpha", "the rate of evolution in slow mode", Validate.REQUIRED);
     public Input<RealParameter> switchRateInput = new Input<RealParameter>("switchRate", "the rate of flipping between slow and fast modes", Validate.REQUIRED);
     public Input<RealParameter> frequenciesInput = new Input<RealParameter>("vfrequencies", "the frequencies of the visible states", Validate.REQUIRED);
-    public Input<RealParameter> hfrequenciesInput = new Input<RealParameter>("hfrequencies", "the frequencies of the hidden rates", Validate.REQUIRED);
+    public Input<RealParameter> hfrequenciesInput = new Input<RealParameter>("hfrequencies", "the frequencies of the hidden rates");
+
+    public enum MODE {BEAST, REVERSIBLE, TUFFLEYSTEEL};
+	public Input<MODE> modeInput = new Input<>("mode","one of BEAST, REVERSIBLE, TUFFLESTEEL "
+			+ "BEAST = implementation as in BEAST 1 "
+			+ "REVERSIBLE = like BEAST 1 implementation, but using frequencies to make it reversible "
+			+ "TUFFLEYSTEEL = Tuffley & Steel (1996) impementation (no rates for ", MODE.BEAST);
 
     private RealParameter alpha;
     private RealParameter switchRate;
@@ -40,6 +83,7 @@ public class BinaryCovarion extends GeneralSubstitutionModel {
     protected double[][] unnormalizedQ;
     protected double[][] storedUnnormalizedQ;
     int stateCount;
+    MODE mode  = modeInput.get();
 
     public BinaryCovarion() {
         ratesInput.setRule(Validate.OPTIONAL);
@@ -53,8 +97,15 @@ public class BinaryCovarion extends GeneralSubstitutionModel {
         frequencies = frequenciesInput.get();
         hiddenFrequencies = hfrequenciesInput.get();
 
-        if (alpha.getDimension() != 1) {
-            throw new Exception("alpha should have dimension 1");
+        
+        if (mode.equals(MODE.BEAST) || mode.equals(MODE.REVERSIBLE)) {
+        	if (alpha.getDimension() != 1) {
+        		throw new Exception("alpha should have dimension 1");
+        	}
+        } else {
+        	if (alpha.getDimension() != 2) {
+        		throw new Exception("alpha should have dimension 2");
+        	}
         }
         if (switchRate.getDimension() != 1) {
             throw new Exception("switchRate should have dimension 1");
@@ -62,10 +113,21 @@ public class BinaryCovarion extends GeneralSubstitutionModel {
         if (frequencies.getDimension() != 2) {
             throw new Exception("frequencies should have dimension 2");
         }
-        if (hiddenFrequencies.getDimension() != 2) {
-            throw new Exception("hiddenFrequenciesshould have dimension 2");
+        if (mode.equals(MODE.BEAST) || mode.equals(MODE.REVERSIBLE)) {
+        	if (hfrequenciesInput.get() == null) {
+        		throw new Exception("hiddenFrequenciesshould should be specified");
+        	}
+            if (hiddenFrequencies.getDimension() != 2) {
+                throw new Exception("hiddenFrequenciesshould have dimension 2");
+            }
+        } else {
+        	if (hfrequenciesInput.get() != null) {
+        		Log.warning.println("WARNING: hfrequencies is specified, but the BinaryCovarion model ignores it.");
+        	}
         }
 
+        
+        
         nrOfStates = 4;
         unnormalizedQ = new double[4][4];
         storedUnnormalizedQ = new double[4][4];
@@ -88,7 +150,6 @@ public class BinaryCovarion extends GeneralSubstitutionModel {
     protected void setupRelativeRates() {
     }
 
-    ;
 
     @Override
     protected void setupRateMatrix() {
@@ -122,41 +183,112 @@ public class BinaryCovarion extends GeneralSubstitutionModel {
     @Override
     public double[] getFrequencies() {
         double[] fFreqs = new double[4];
-        fFreqs[0] = frequencies.getValue(0) * hiddenFrequencies.getValue(0);
-        fFreqs[1] = frequencies.getValue(1) * hiddenFrequencies.getValue(0);
-        fFreqs[2] = frequencies.getValue(0) * hiddenFrequencies.getValue(1);
-        fFreqs[3] = frequencies.getValue(1) * hiddenFrequencies.getValue(1);
+        if (mode.equals(MODE.BEAST) || mode.equals(MODE.REVERSIBLE)) {
+	        fFreqs[0] = frequencies.getValue(0) * hiddenFrequencies.getValue(0);
+	        fFreqs[1] = frequencies.getValue(1) * hiddenFrequencies.getValue(0);
+	        fFreqs[2] = frequencies.getValue(0) * hiddenFrequencies.getValue(1);
+	        fFreqs[3] = frequencies.getValue(1) * hiddenFrequencies.getValue(1);
+        } else {
+        	double h0 = alpha.getValue(1) * (alpha.getValue(0) + alpha.getValue(1));
+        	double h1 = alpha.getValue(0) * (alpha.getValue(0) + alpha.getValue(1));
+	        fFreqs[0] = frequencies.getValue(0) * h0;
+	        fFreqs[1] = frequencies.getValue(1) * h0;
+	        fFreqs[2] = frequencies.getValue(0) * h1;
+	        fFreqs[3] = frequencies.getValue(1) * h1;        	
+        }
         return fFreqs;
     }
 
 
     protected void setupUnnormalizedQMatrix() {
 
-        double a = alpha.getValue(0);
-        double s = switchRate.getValue(0);
-        double f0 = hiddenFrequencies.getValue(0);
-        double f1 = hiddenFrequencies.getValue(1);
-        double p0 = frequencies.getValue(0);
-        double p1 = frequencies.getValue(1);
+        switch (mode) {
+        case BEAST: {
 
-        assert Math.abs(1.0 - f0 - f1) < 1e-8;
-        assert Math.abs(1.0 - p0 - p1) < 1e-8;
+            double a = alpha.getValue(0);
+            double s = switchRate.getValue(0);
+            double f0 = hiddenFrequencies.getValue(0);
+            double f1 = hiddenFrequencies.getValue(1);
+            double p0 = frequencies.getValue(0);
+            double p1 = frequencies.getValue(1);
 
-        unnormalizedQ[0][1] = a * p1;
-        unnormalizedQ[0][2] = s;
-        unnormalizedQ[0][3] = 0.0;
+            assert Math.abs(1.0 - f0 - f1) < 1e-8;
+            assert Math.abs(1.0 - p0 - p1) < 1e-8;
 
-        unnormalizedQ[1][0] = a * p0;
-        unnormalizedQ[1][2] = 0.0;
-        unnormalizedQ[1][3] = s;
+            unnormalizedQ[0][1] = a * p1;
+            unnormalizedQ[0][2] = s;
+            unnormalizedQ[0][3] = 0.0;
 
-        unnormalizedQ[2][0] = s;
-        unnormalizedQ[2][1] = 0.0;
-        unnormalizedQ[2][3] = p1;
+            unnormalizedQ[1][0] = a * p0;
+            unnormalizedQ[1][2] = 0.0;
+            unnormalizedQ[1][3] = s;
 
-        unnormalizedQ[3][0] = 0.0;
-        unnormalizedQ[3][1] = s;
-        unnormalizedQ[3][2] = p0;
+            unnormalizedQ[2][0] = s;
+            unnormalizedQ[2][1] = 0.0;
+            unnormalizedQ[2][3] = p1;
+
+            unnormalizedQ[3][0] = 0.0;
+            unnormalizedQ[3][1] = s;
+            unnormalizedQ[3][2] = p0;
+        }
+        break;
+        case REVERSIBLE: {
+
+            double a = alpha.getValue(0);
+            double s = switchRate.getValue(0);
+            double f0 = hiddenFrequencies.getValue(0);
+            double f1 = hiddenFrequencies.getValue(1);
+            double p0 = frequencies.getValue(0);
+            double p1 = frequencies.getValue(1);
+
+            assert Math.abs(1.0 - f0 - f1) < 1e-8;
+            assert Math.abs(1.0 - p0 - p1) < 1e-8;
+
+            unnormalizedQ[0][1] = a * p1 * f0;
+            unnormalizedQ[0][2] = s * f0;
+            unnormalizedQ[0][3] = 0.0;
+
+            unnormalizedQ[1][0] = a * p0 * f0;
+            unnormalizedQ[1][2] = 0.0;
+            unnormalizedQ[1][3] = s * f0;
+
+            unnormalizedQ[2][0] = s * f1;
+            unnormalizedQ[2][1] = 0.0;
+            unnormalizedQ[2][3] = p1 * f1;
+
+            unnormalizedQ[3][0] = 0.0;
+            unnormalizedQ[3][1] = s * f1;
+            unnormalizedQ[3][2] = p0 * f1;
+        }
+    	break;
+        case TUFFLEYSTEEL: {
+            double a = alpha.getValue(0);
+            double s1 = switchRate.getValue(0);
+            double s2 = switchRate.getValue(0);
+            double p0 = frequencies.getValue(0);
+            double p1 = frequencies.getValue(1);
+
+            assert Math.abs(1.0 - p0 - p1) < 1e-8;
+
+            unnormalizedQ[0][1] = a * p1;
+            unnormalizedQ[0][2] = s1;
+            unnormalizedQ[0][3] = 0.0;
+
+            unnormalizedQ[1][0] = a * p0;
+            unnormalizedQ[1][2] = 0.0;
+            unnormalizedQ[1][3] = s1;
+
+            unnormalizedQ[2][0] = s2;
+            unnormalizedQ[2][1] = 0.0;
+            unnormalizedQ[2][3] = p1;
+
+            unnormalizedQ[3][0] = 0.0;
+            unnormalizedQ[3][1] = s2;
+            unnormalizedQ[3][2] = p0;
+        }
+       	break;
+        }
+
     }
 
     /**
