5950ac9754f9db2a6a03a4654829cc386c189797
Tim Vaughan <tgvaughan@gmail.com>
2015-11-13 16:23:00 +1300

    Revert "Closes issue #438 on RandomTree.  Thanks @jheled!"
    This reverts commit f4b08388bbd0c428843b62e2e84af13203af4cab.

--- src/beast/evolution/tree/RandomTree.java
+++ src/beast/evolution/tree/RandomTree.java
@@ -304,22 +304,8 @@ public class RandomTree extends Tree implements StateNodeInitialiser {
         internalNodeCount = sTaxa.size() - 1;
         leafNodeCount = sTaxa.size();
 
-        HashMap<String,Integer> taxonToNR = null;
-        // preserve node numbers where possible
-        if (m_initial.get() != null) {
-            taxonToNR = new HashMap<>();
-            for( Node n : m_initial.get().getExternalNodes() ) {
-                taxonToNR.put(n.getID(), n.getNr());
-            }
-        } else {
-            taxonToNR = new HashMap<>();
-            String[] taxa = getTaxaNames();
-            for(int k = 0; k < taxa.length; ++k) {
-                taxonToNR.put(taxa[k], k);
-            }
-        }
         // multiple simulation tries may produce an excess of nodes with invalid nr's. reset those.
-        setNodesNrs(root, 0, new int[1], taxonToNR);
+        setNodesNrs(root, 0, new int[1]);
 
         initArrays();
 
@@ -338,24 +324,19 @@ public class RandomTree extends Tree implements StateNodeInitialiser {
         }
     }
 
-    private int setNodesNrs(final Node node, int internalNodeCount, int[] n, Map<String,Integer> initial) {
+    private int setNodesNrs(final Node node, int internalNodeCount, int[] n) {
         if( node.isLeaf() )  {
-            if( initial != null ) {
-                node.setNr(initial.get(node.getID()));
-            } else {
-                node.setNr(n[0]);
-                n[0] += 1;
-            }
+            node.setNr(n[0]);
+            n[0] += 1;
         } else {
             for (final Node child : node.getChildren()) {
-                internalNodeCount = setNodesNrs(child, internalNodeCount, n, initial);
+                internalNodeCount = setNodesNrs(child, internalNodeCount, n);
             }
             node.setNr(nrOfTaxa + internalNodeCount);
             internalNodeCount += 1;
         }
         return internalNodeCount;
     }
-
 
     private void scaleToFit(double scale, Node node) {
         if (!node.isLeaf()) {
