a23f4deb77c99116bae1ec50961c7ae6afa34803
Remco Bouckaert <higgs.ml@gmail.com>
2017-04-27 07:43:51 +1200

    update version to 2.4.6 #685

--- src/beast/app/BEASTVersion2.java
+++ src/beast/app/BEASTVersion2.java
@@ -9,7 +9,7 @@ public class BEASTVersion2 extends BEASTVersion {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "2.4.5";
+    private static final String VERSION = "2.4.6";
 
     private static final String DATE_STRING = "2002-2017";
 
