c2c20288d5644e414c561914bc7991d91da8a374
Walter <walterxie@users.noreply.github.com>
2017-05-08 14:49:09 +1200

    GUI tests were randomly failed #696

--- src/test/beast/app/beauti/BeautiRateTutorialTest.java
+++ src/test/beast/app/beauti/BeautiRateTutorialTest.java
@@ -3,34 +3,21 @@ package test.beast.app.beauti;
 
 
 
-import static org.fest.swing.edt.GuiActionRunner.execute;
-import static org.fest.swing.finder.JFileChooserFinder.findFileChooser;
-
-import java.awt.Component;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.swing.JComboBox;
-
+import beast.app.util.Utils;
 import org.fest.swing.data.Index;
 import org.fest.swing.data.TableCell;
 import org.fest.swing.edt.GuiTask;
-import org.fest.swing.fixture.JButtonFixture;
-import org.fest.swing.fixture.JCheckBoxFixture;
-import org.fest.swing.fixture.JComboBoxFixture;
-import org.fest.swing.fixture.JFileChooserFixture;
-import org.fest.swing.fixture.JOptionPaneFixture;
-import org.fest.swing.fixture.JTabbedPaneFixture;
-import org.fest.swing.fixture.JTableCellFixture;
-import org.fest.swing.fixture.JTableFixture;
+import org.fest.swing.fixture.*;
 import org.fest.swing.image.ScreenshotTaker;
 import org.junit.Test;
 
-import beast.app.util.Utils;
-import test.beast.beast2vs1.TestFramework;
-import test.beast.beast2vs1.trace.Expectation;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.Arrays;
+
+import static org.fest.swing.edt.GuiActionRunner.execute;
+import static org.fest.swing.finder.JFileChooserFinder.findFileChooser;
 
 
 
@@ -240,68 +227,12 @@ public class BeautiRateTutorialTest extends BeautiBase {
 		//7. Run MCMC and look at results in Tracer, TreeAnnotator->FigTree
 		warning("7. Run MCMC and look at results in Tracer, TreeAnnotator->FigTree");
 		makeSureXMLParses();
-		
-		// TODO: this should run as a separate process since the BEAUti run can interfere with the BEAST run on Hudson
- 		//MEPRunner runner = new MEPRunner(org.fest.util.Files.temporaryFolder());
- 		//runner.analyse(0);
-		
+
 		long t1 = System.currentTimeMillis();
 		System.err.println("total time: " + (t1 - t0)/1000 + " seconds");
 		
 	}
-	
-	
-	 // This is for debugging the test only
-// 	MEPRunner should be run from MEPTutorial()
-// 	@Test
-// 	public void runXML() throws Exception {
-// 		//System.setProperty("file.name.prefix", org.fest.util.Files.temporaryFolder().getAbsolutePath());
-// 		MEPRunner runner = new MEPRunner(org.fest.util.Files.temporaryFolder());
-// 		runner.analyse(0);
-// 		
-// 	}
-    
-	class MEPRunner extends TestFramework {
-		
-		MEPRunner(File file) {
-			super();
-			setUp(new String[]{"/x.xml"});
-			dirName = file.getPath();
-			logDir = "";
-			useSeed = false;
-	        checkESS = false;
-			testFile = "RSV2";
-		}
 
-		@Override
-		protected List<Expectation> giveExpectations(int index_XML) throws Exception {
-	        List<Expectation> expList = new ArrayList<Expectation>();
-	        addExpIntoList(expList,"posterior", -6131.89, 0.922052);
-	        addExpIntoList(expList,"likelihood", -5496.28, 0.401133);
-	        // low ESS for seed=128
-	        addExpIntoList(expList,"prior", -635.603, 1.215535);
-	        addExpIntoList(expList,"treeLikelihood.1", -1440.16, 0.197223);
-	        addExpIntoList(expList,"treeLikelihood.3", -2271.52, 0.300608);
-	        addExpIntoList(expList,"treeLikelihood.2", -1784.59, 0.29738);
-	        addExpIntoList(expList,"TreeHeight", 56.06136, 0.125308);
-	        addExpIntoList(expList,"kappa.1", 7.727761, 0.069897);
-	        addExpIntoList(expList,"kappa.2", 10.41839, 0.093578);
-	        addExpIntoList(expList,"kappa.3", 11.97769, 0.090429);
-	        addExpIntoList(expList,"mutationRate.1", 0.698603, 0.000868);
-	        addExpIntoList(expList,"mutationRate.2", 0.960092, 0.00099);
-	        addExpIntoList(expList,"mutationRate.3", 1.33987, 0.001132);
-	        addExpIntoList(expList,"clockRate", 0.002179, 1.16E-5);
-	        addExpIntoList(expList,"popSize", 37.44745, 0.368656);
-	        // low ESS for seed=128
-	        addExpIntoList(expList,"CoalescentConstant", -590.862, 1.164024);
-			return expList;
-		}
-		
-	}
-    
-
-
-	
 	@Test
 	public void MEPBSPTutorial() throws InterruptedException {
 		if (true) {return;}
---
+++ src/test/beast/beast2vs1/tutorials/DivergenceDatingTest.java
@@ -0,0 +1,79 @@
+package test.beast.beast2vs1.tutorials;
+
+
+import org.junit.Test;
+import test.beast.beast2vs1.TestFramework;
+import test.beast.beast2vs1.trace.Expectation;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DivergenceDatingTest {
+    @Test
+    public void DivergenceDatingTutorial() throws Exception {
+        DivergenceDatingRunner runner = new DivergenceDatingRunner(org.fest.util.Files.temporaryFolder());
+        runner.analyse(0);
+    }
+
+    // This is for debugging the test only
+// 	DivergenceDatingRunner should be run from DivergenceDatingTutorial()
+// 	@Test
+// 	public void runXML() throws Exception {
+// 		//System.setProperty("file.name.prefix", org.fest.util.Files.temporaryFolder().getAbsolutePath());
+// 		DivergenceDatingRunner runner = new DivergenceDatingRunner(org.fest.util.Files.temporaryFolder());
+// 		runner.analyse(0);
+//
+// 	}
+
+    class DivergenceDatingRunner extends TestFramework {
+
+        DivergenceDatingRunner(File file) {
+            super();
+            setUp(new String[]{"/x.xml"});
+            dirName = file.getPath();
+            logDir = "";
+            useSeed = false;
+            checkESS = false;
+            testFile = "primate-mtDNA";
+            SEED = 126;
+        }
+
+        @Override
+        protected List<Expectation> giveExpectations(int index_XML) throws Exception {
+            List<Expectation> expList = new ArrayList<Expectation>();
+            addExpIntoList(expList,"posterior", -5508.64, 0.277076);
+            addExpIntoList(expList,"likelihood", -5442.24, 0.314717);
+            addExpIntoList(expList,"prior", -67.5441, 0.197599);
+            addExpIntoList(expList,"treeLikelihood.1stpos", -1382.86, 0.163746);
+            // low ESS for seed=128
+            addExpIntoList(expList,"treeLikelihood.noncoding", -957.075, 0.157176);
+            addExpIntoList(expList,"treeLikelihood.2ndpos", -954.148, 0.184448);
+            addExpIntoList(expList,"treeLikelihood.3rdpos", -2148.15, 0.311767);
+            addExpIntoList(expList,"TreeHeight", 83.46231, 1.039008);
+            addExpIntoList(expList,"YuleModel", -51.2849, 0.115309);
+            addExpIntoList(expList,"birthRate", 0.029973, 0.000342);
+            addExpIntoList(expList,"kappa.noncoding", 14.67406, 0.462209);
+            addExpIntoList(expList,"kappa.1stpos", 6.812315, 0.113013);
+            addExpIntoList(expList,"kappa.2ndpos", 8.853521, 0.19871);
+            // low ESS for seed=128
+            addExpIntoList(expList,"kappa.3rdpos", 30.52025, 0.772299);
+            addExpIntoList(expList,"gammaShape.noncoding", 0.241535, 0.005483);
+            addExpIntoList(expList,"gammaShape.1stpos", 0.480865, 0.006024);
+            addExpIntoList(expList,"gammaShape.2ndpos", 0.576606, 0.017974);
+            addExpIntoList(expList,"gammaShape.3rdpos", 2.832824, 0.092259);
+            // low ESS for seed=128
+            addExpIntoList(expList,"mutationRate.noncoding", 0.12345, 0.003921);
+            // low ESS for seed=128
+            addExpIntoList(expList,"mutationRate.1stpos", 0.157503, 0.002944);
+            // low ESS for seed=128
+            addExpIntoList(expList,"mutationRate.2ndpos", 0.061211, 0.001608);
+            addExpIntoList(expList,"logP(mrca(Human-Chimp))", -0.78481, 0.022675);
+            addExpIntoList(expList,"mrcatime(Human-Chimp)", 5.845026, 0.014885);
+            addExpIntoList(expList,"clockRate", 0.034266, 0.000481);
+            return expList;
+        }
+
+    }
+
+}
