38c4bdb3629dd6bb99236abf9323699c2053341e
Remco Bouckaert <higgs.ml@gmail.com>
2018-09-06 15:35:23 +1200

    update DensiTree and version numbers for v2.5.1 #798

--- src/beast/app/BEASTVersion2.java
+++ src/beast/app/BEASTVersion2.java
@@ -9,7 +9,7 @@ public class BEASTVersion2 extends BEASTVersion {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "2.5.0";
+    private static final String VERSION = "2.5.1";
 
     private static final String DATE_STRING = "2002-2018";
 
