620351b666d3e295c865fa4477a553bf74a01f25
rbouckaert <higgs.ml@gmail.com>
2021-07-30 11:00:31 +1200

    Revert "Use instanceof instead of try clause"
    This reverts commit 7489e73ed607d4d63f7c1bf24f5b399f7fb83535.
    Because it causes an API change, so we reserve it for v2.7.0

--- src/beast/evolution/branchratemodel/StrictClockModel.java
+++ src/beast/evolution/branchratemodel/StrictClockModel.java
@@ -12,45 +12,47 @@ import beast.evolution.tree.Node;
 @Description("Defines a mean rate for each branch in the beast.tree.")
 public class StrictClockModel extends BranchRateModel.Base {
 
-    // public Input<RealParameter> muParameterInput = new Input<>("clock.rate", "the
-    // clock rate (defaults to 1.0)");
+	// public Input<RealParameter> muParameterInput = new Input<>("clock.rate", "the
+	// clock rate (defaults to 1.0)");
 
-    protected Function muSource;
-    private double mu = 1.0;
+	protected Function muSource;
+	private double mu = 1.0;
 
-    @Override
-    public void initAndValidate() {
-        if (meanRateInput.get() != null) {
-            muSource = meanRateInput.get();
-            if (muSource instanceof RealParameter) {
-                RealParameter muParameter = (RealParameter) muSource;
-                muParameter.setBounds(Math.max(0.0, muParameter.getLower()), muParameter.getUpper());
-            }
-            mu = muSource.getArrayValue();
-        }
-    }
+	@Override
+	public void initAndValidate() {
+		if (meanRateInput.get() != null) {
+			muSource = meanRateInput.get();
+			try {
+				RealParameter muParameter = (RealParameter) muSource;
+				muParameter.setBounds(Math.max(0.0, muParameter.getLower()), muParameter.getUpper());
+			} catch (ClassCastException t) {
+				// Nothing
+			}
+			mu = muSource.getArrayValue();
+		}
+	}
 
-    @Override
-    public double getRateForBranch(final Node node) {
-        return mu;
-    }
+	@Override
+	public double getRateForBranch(final Node node) {
+		return mu;
+	}
 
-    @Override
-    public boolean requiresRecalculation() {
-        mu = muSource.getArrayValue();
-        return true;
-    }
+	@Override
+	public boolean requiresRecalculation() {
+		mu = muSource.getArrayValue();
+		return true;
+	}
 
-    @Override
-    protected void restore() {
-        mu = muSource.getArrayValue();
-        super.restore();
-    }
+	@Override
+	protected void restore() {
+		mu = muSource.getArrayValue();
+		super.restore();
+	}
 
-    @Override
-    protected void store() {
-        mu = muSource.getArrayValue();
-        super.store();
-    }
+	@Override
+	protected void store() {
+		mu = muSource.getArrayValue();
+		super.store();
+	}
 
 }
