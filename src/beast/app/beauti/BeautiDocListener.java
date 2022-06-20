a210a4921e1c40054476da24df96a5c052481d58
Remco Bouckaert <higgs.ml@gmail.com>
2022-06-21 08:39:26 +1200

    purge for v2.7 candidate

--- src/beast/app/beauti/BeautiDocListener.java
+++
@@ -1,9 +0,0 @@
-package beast.app.beauti;
-
-/**
- * @author Andrew Rambaut
- * @version $Id$
- */
-public interface BeautiDocListener {
-    void docHasChanged() throws Exception;
-}
--- src/beast/core/util/Evaluator.java
+++
@@ -1,9 +0,0 @@
-package beast.core.util;
-
-/**
- * @author Andrew Rambaut
- * @version $Id$
- */
-public interface Evaluator {
-    double evaluate();
-}
