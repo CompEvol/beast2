e71bd4345d8a6b6ae143ceb4bd1ef09e744a1061
rbouckaert <higgs.ml@gmail.com>
2023-08-03 15:15:33 +1200

    robustify Tree so translate block is printed correctly up to 999999 tax. fixes #1122

--- src/beast/base/evolution/tree/Tree.java
+++ src/beast/base/evolution/tree/Tree.java
@@ -683,7 +683,7 @@ public class Tree extends StateNode implements TreeInterface {
      */
     static void printTranslate(Node node, List<String> translateLines, int nodeCount) {
         if (node.isLeaf()) {
-            String line = String.format("\t\t%4d ", node.getNr() + taxaTranslationOffset);
+            String line = String.format("\t\t%6d ", node.getNr() + taxaTranslationOffset);
             if (node.getID().indexOf(' ') > 0) {
             	char c = node.getID().charAt(0);
             	if (c == '\"' || c == '\'') {
